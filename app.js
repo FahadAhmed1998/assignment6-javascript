6526d53778ad2cd32adaf1cf5552b272d78f5072
FahadAhmed1086 <fahadahmed1086@gmail.com>
2020-06-18 18:42:56 +0500

    app.js changed

---
+++ app.js
@@ -0,0 +1,123 @@
+
+//////////////////////////////////////////////////////////////Chapter 21-25//////////////////////////////////////////////////////////
+/*
+//task1//
+var firstName=prompt("Enter first name");
+var lastName=prompt("Enter last name");
+var fullName=firstName + lastName;
+alert("Hello " + fullName);
+*/
+
+
+/*
+//task2//
+var favPhone=prompt("Enter your favorite mobile phone model");
+document.write("My favorite phone is: " + favPhone);
+str=favPhone.length;
+document.write("<br\>Length of string: " + str);
+*/
+
+
+
+/*
+//task3//
+var text="Pakistani"
+var res=text.indexOf('n');
+document.write("String: " + text);
+document.write("<br\>Index of 'n': " + res);
+*/
+
+/*
+//task4//
+var text="Hello World"
+var res=text.lastIndexOf('l');
+document.write("String: " + text);
+document.write("<br\>Last Index of 'l': " + res);
+*/
+
+/*
+//task5//
+var text="Pakistani"
+var res=text.charAt(3);
+document.write("String: " + text);
+document.write("<br\>Character at index 3: " + res);
+*/
+
+
+
+/*
+//task6//
+var str1=prompt("Enter first name");
+var str2=prompt("Enter last name");
+var fullName=str1.concat(str2);
+alert("Hello " + fullName);
+*/
+
+/*
+//task7//
+var text="Hyderabad";
+var res=text.replace("Hyder","Islam");
+document.write("City: " + text);
+document.write("<br\>After replacement: " + res );
+*/
+
+
+
+/*
+//task8//
+var text="Ali and Sami are best friends. They play cricket and football together";
+var res=text.replace("and","&");
+document.write("Message: " + text);
+document.write("<br\>After replacement: " + res );
+*/
+
+
+/*
+//task9//
+*/
+
+/*
+//task10//
+var text="peanuts";
+var res=text.toUpperCase(text);
+document.write("User input: " + text);
+document.write("<br\>Upper case: " + res );
+*/
+
+/*
+//task11//
+*/
+
+/*
+//task12//
+*/
+
+/*
+//task13//
+*/
+
+/*
+//task14//
+*/
+
+/*
+//task15//
+*/
+
+/*
+//task16//
+*/
+
+/*
+//task17//
+var text="Pakistan";
+var res=text.charAt(7);
+document.write("User input: " + text);
+document.write("<br\>Last character of input: " + res);
+*/
+
+/*
+//task18//
+var text="The quick brown fox jumps over the lazy dog";
+document.write("Text: " + text);
+*/
